e8e428ed55f55160b9afe092f91116c3314ea2a8
Deserdhod <32869477+Deserdhod@users.noreply.github.com>
2018-01-22 13:56:13 +0700

    LS
    none

--- AgainGame/android/src/com/mygdx/game/AndroidLauncher.java
+++ AgainGame/android/src/com/mygdx/game/AndroidLauncher.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
 		config.useCompass = false;
-		initialize(new GameScreen(), config);
+		initialize(new Drop(), config);
 	}
 }
